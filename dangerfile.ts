cde1bf57739f92c37ac6839d586de78f9d0471bd
Marco Cotrufo <marco.cotrufo@devncode.it>
2021-12-13 08:33:30 +0100

    ci(danger): checks for correct pr title & edited files

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,23 @@
 import { danger, fail, schedule } from "danger"
-import { isEqual, includes } from "lodash";
+import { isEqual, includes } from "lodash"
+import eslint from "@fido-id/danger-plugin-eslint"
+import commitlint from "danger-plugin-conventional-commitlint"
+import configConventional from "@commitlint/config-conventional"
 
 schedule(async () => {
+  // checks for eslint errors
+  eslint()
+
+  const hasChangelogModified = danger.git.modified_files.includes(
+    "docs/CHANGELOG.md",
+  )
+
+  if (hasChangelogModified) {
+    fail("Please do not edit the changelog! It is automatically generated.")
+  }
+
+  await commitlint(configConventional.rules, { severity: "warn" })
+
   const packageDiff = await danger.git.JSONDiffForFile("package.json")
 
   const haveDependenciesChanged =
